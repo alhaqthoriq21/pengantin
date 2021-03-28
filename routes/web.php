e3dcb684e17bab74bf653fd8305d58c2de1e2873
alhaqqthoriq21 <alhaqthoriq25@gmail.com>
2021-03-28 11:45:05 +0700

    update new

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,9 @@ use App\Http\Controllers\SongController;
         return view('auth/login');
     });
 
-    Auth::routes();
+    // Auth::routes();
 
-    Route::get('/login/dashboard', [App\Http\Controllers\CalonController::class, 'index'])->name('home');
+    // Route::get('/login/dashboard', [App\Http\Controllers\CalonController::class, 'index'])->name('home');
 
     //routing slug
     Route::get("/{slug}", [ProjectController::class, "getData"])->name("get.project.data");
